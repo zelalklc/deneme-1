d9b210b87a86b19ea7f965691fe54d19bbb8a3cd
zelal <zelalklc11@gmail.com>
2024-07-12 18:25:00 +0300

    Delete rrandom.py

--- rrandom.py
+++
@@ -1,5 +0,0 @@
-import random
-liste_tshirt = ["animeli t-shirt","turuncu t-shirt","yine beyaz t-shirt","2. animeli t-shirt","yeşil gömlekli kombin"]
-print(random.choice(liste_tshirt))
-liste_pant= ["siyh paraşüt","yeşil paraşüt","mavi kot","siyah kot","nike eşofman","adidas eşofman"]
-print(random.choice(liste_pant))
