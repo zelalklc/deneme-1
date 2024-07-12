3f4e7ecde7ccc98009a48f2e14fe2cf97619f280
zelal <zelalklc11@gmail.com>
2024-07-12 18:08:49 +0300

    random.py
    update

--- rrandom.py
+++ rrandom.py
@@ -3,3 +3,17 @@ liste_tshirt = ["animeli t-shirt","turuncu t-shirt","yine beyaz t-shirt","2. ani
 print(random.choice(liste_tshirt))
 liste_pant= ["siyh paraşüt","yeşil paraşüt","mavi kot","siyah kot","nike eşofman","adidas eşofman"]
 print(random.choice(liste_pant))
+
+while True:
+    kullanici=input("Başlayalım o halde; taş mı, kağıt mı, makas mı?").lower()
+
+    bot = ['taş','kağıt','makas']
+    bot = random.choices(bot)
+    if kullanici == bot:
+        print("EŞİTTTT!!!")
+    elif (kullanici == 'taş' and bot == 'makas')or \
+         (kullanici == 'kağıt' and bot == 'taş')or \
+         (kullanici == 'makas' and bot == 'kağıt'):
+        print("bilgisayar:",bot,"'ı seçti.Sen kazandın mutlu musun...")
+    else:
+        print("bilgisayar:",bot,"'ı seçti.Sen kaybettin.Başka zamana.")
